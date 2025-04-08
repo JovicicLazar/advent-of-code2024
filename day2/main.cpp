cad95d0c70b05ce2e6f0075af0d9b5f6903c146a
JovicicLazar <lazarjovicic808@gmail.com>
2025-04-09 00:18:28 +0200

    added day2 solution part 2

--- day2/main.cpp
+++ day2/main.cpp
@@ -6,16 +6,18 @@
 
 namespace {
     const std::string INPUT_FILE_NAME = "puzzle_input.txt";
+    const size_t REPORT_NUMBER        = 1000;
 }
 
 std::vector<std::vector<int>> load_data(const std::string& path_to_input) {
     std::ifstream input_file(path_to_input);
     if (!input_file.is_open()) {
         std::cerr << "Unable to open file: " << path_to_input << std::endl;
-        return {{}};
+        return {};
     }
 
     std::vector<std::vector<int>> reports;
+    reports.reserve(REPORT_NUMBER);
     std::string line;
 
     while (std::getline(input_file, line)) {
@@ -34,37 +36,65 @@ std::vector<std::vector<int>> load_data(const std::string& path_to_input) {
 }
 
 bool is_good_report(const std::vector<int>& report) {
-    int n        = report.size();
-    bool is_incr = (report[0] < report[n - 1])? true : false;
+    const bool is_increasing         = report.front() < report.back();
+    const auto& [min_diff, max_diff] = is_increasing ? std::make_pair(1, 3) : std::make_pair(-3, -1);
 
-    for(int i = 1; i < report.size(); ++i) {
-        int diff = report[i] - report[i - 1]; 
-        if(is_incr) {
-            if( diff < 1 || diff > 3) {
-                return false;
-            }
-        } else if(!is_incr) {
-            if( diff > -1 || diff < -3) {
-                return false;
-            }
+    for (size_t i = 1; i < report.size(); ++i) {
+        int diff = report[i] - report[i - 1];
+        if (diff < min_diff || diff > max_diff) {
+            return false;
         }
     }
     return true;
 }
 
+bool is_good_report_with_dampener(const std::vector<int>& report) {
+    if (is_good_report(report)) return true;
+    
+    if (report.size() < 3) return true;
+
+    for (size_t i = 0; i < report.size(); ++i) {
+        std::vector<int> temp;
+        temp.reserve(report.size() - 1);
+        
+        for (size_t j = 0; j < report.size(); ++j) {
+            if (j != i) {
+                temp.push_back(report[j]);
+            }
+        }
+        
+        if (is_good_report(temp)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 void part1(const std::string& path_to_input) {
     auto reports     = load_data(path_to_input);
     int good_reports = 0;
-    bool inc         = false;
 
     for(const std::vector<int>& report : reports) {
-        if(is_good_report(report)) good_reports++;
+        good_reports += is_good_report(report);
     }
 
     std::cout << good_reports << std::endl;
 }
 
+void part2(const std::string& path_to_input) {
+    auto reports     = load_data(path_to_input);
+    int good_reports = 0;
+
+    for(std::vector<int>& report : reports) {
+        good_reports += is_good_report_with_dampener(report);
+    }
+
+    std::cout << good_reports << std::endl;
+}
+
+
 int main() {
     part1(INPUT_FILE_NAME);
+    part2(INPUT_FILE_NAME);
     return 0;
 } 
